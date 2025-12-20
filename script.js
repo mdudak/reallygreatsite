6b6752c1ace31f93de27cb25f297d9204aab7597
MDudak <mato.dudak@gmail.com>
2025-12-20 12:42:10 +0100

    Prvotna stranka

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const reallyGreatDate = new Date(2026, 3, 11, 15, 0, 0)
 
+function pluralize(value, forms) {
+    // forms = [1, 2-4, 5+]
+    if (value === 1) return forms[0]
+    if (value >= 2 && value <= 4) return forms[1]
+    return forms[2]
+}
+
 function countdown() {
     const element = document.getElementById('countdown')
 
@@ -15,10 +22,14 @@ function countdown() {
     const hours = Math.floor(diff / (1000 * 60 * 60)) % 24
     const days = Math.floor(diff / (1000 * 60 * 60 * 24))
 
-    element.textContent = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`
+    element.textContent =
+        `${days} ${pluralize(days, ['deň', 'dni', 'dní'])} ` +
+        `${hours} ${pluralize(hours, ['hodina', 'hodiny', 'hodín'])} ` +
+        `${minutes} ${pluralize(minutes, ['minúta', 'minúty', 'minút'])} ` +
+        `${seconds} ${pluralize(seconds, ['sekunda', 'sekundy', 'sekúnd'])}`
 }
 
-addEventListener("DOMContentLoaded", (_event) => {
+addEventListener("DOMContentLoaded", () => {
     countdown()
     setInterval(countdown, 1000)
 })
